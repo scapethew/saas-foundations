07cbd53bc86646687628eb44d8b39bbd4e7b290d
Matthew Martin <matthew@hellois0.com>
2024-10-12 10:39:33 -0500

    Django Templates Intro

--- src/projhome/views.py
+++ src/projhome/views.py
@@ -1,11 +1,30 @@
 import pathlib
+from django.shortcuts import render
 from django.http import HttpResponse
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-    print(this_dir)
-    html_ = ""
-    html_file_path = this_dir / "home.html"
-    html_ = html_file_path.read_text()
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "home.html"
+    return render(request, html_template, my_context)
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+    <body>
+        <h1> {page_title} anything? </h1>
+    </body>
+</html>
+""".format(**my_context)
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
     return HttpResponse(html_)
