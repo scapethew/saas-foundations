79813d70f73795a429ca19d08220f9cf7c0422da
Matthew Martin <matthew@hellois0.com>
2024-10-12 10:19:38 -0500

    Rendering HTML pages

--- src/projhome/views.py
+++ src/projhome/views.py
@@ -1,6 +1,11 @@
+import pathlib
 from django.http import HttpResponse
 
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-    
-    return HttpResponse("<h1>Hello World</h1>")
+    print(this_dir)
+    html_ = ""
+    html_file_path = this_dir / "home.html"
+    html_ = html_file_path.read_text()
+    return HttpResponse(html_)
